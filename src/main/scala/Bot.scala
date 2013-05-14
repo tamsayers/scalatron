eb4ab23c7e4441ea5a585c06386b7374be0fc810
tam <tamsayers@hotmail.com>
2013-05-14 17:05:53 +0100

    parsed welcome string

--- src/main/scala/Bot.scala
+++ src/main/scala/Bot.scala
@@ -1,7 +1,18 @@
 class ControlFunctionFactory {
-    def create = new ControlFunction().respond _
+  def create = new ControlFunction().respond _
+}
+
+case class Welcome(name: String, path: String, apocalypse: Int, round: Int) {
+  override def toString() = "Name=" + name + "; path=" + path + "; apocalypse=" + apocalypse + "; round=" + round
 }
 
 class ControlFunction {
-    def respond(input: String): String = "Status(text=Hello World)"
+  val WelcomeRegex = "Welcome\\(name=(.+),path=(.+),apocalypse=([0-9]+),round=([0-9]+)\\)".r
+
+  def respond(input: String): String = input match {
+    case WelcomeRegex(name, path, apocalypse, round) => {
+      println(Welcome(name, path, apocalypse.toInt, round.toInt))
+      "Move(Say(text=Hello from " + name + "))"
+    }
+  }
 }
